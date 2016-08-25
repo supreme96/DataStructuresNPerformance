8a6285f3053a43753027c8c9cd1c8a4e3adb2ec1
Sahil Tandon <standon196@gmail.com>
2016-08-25 13:04:59 +0530

    Beginning Project

---
+++ bst.java
@@ -0,0 +1,17 @@
+//Created by Sahil Tandon 
+//The program uses generic datatypes and can be easily changed to support different datatypes.
+
+public class BST{
+	
+
+}
+
+class template<T> node{
+	private T data;
+	public node left;
+	public node right;
+
+	public T getData(){
+		return data;
+	}
+}
