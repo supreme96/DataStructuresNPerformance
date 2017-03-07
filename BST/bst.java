afdf0282622f4229aa27c927fc2dc1a10bb3b51f
Sahil <standon196@gmail.com>
2017-03-07 23:55:53 +0530

    Added code for virtually all the functions of the tree. Functions added are addNode, locateNode, findNode, updateNode, removeNode etc.

--- BST/bst.java
+++ BST/bst.java
@@ -14,24 +14,125 @@ public class bst {
 
 class BST<t> {
 	
-	private	node<t> root;
+	private	Node<t> root;
 
-	public node<t> getRoot(){
+	public Node<t> getRoot(){
 		return root;
 	}
 
 	public void createTree(t data){
-		root = new node<t>();
+		root = new Node<t>();
 		root.setData(data);
 		root.left = null;
 		root.right = null;
 	}
+
+	public void addNode(t data){
+		Node newNode = new Node();
+		newNode.setData(data);
+		Node nodePositionFinder = root;
+		Node parentTracker;
+		boolean leftRightFlag;
+		while(nodeReference != null){
+			parentTracker = nodePositionFinder;
+			if(nodeReference.getData()>=data){
+				nodeReference = nodeReference.left;
+				leftRightFlag = true;	
+			}
+			else{
+				nodeReference = nodeReference.right;
+				leftRightFlag = false;
+			}
+		}
+		if(leftRightFlag == true){
+			parentTracker.left = newNode;
+		}
+		else{
+			parentTracker.right = newNode;
+		}
+		newNode = null;
+		nodePositionFinder = null;
+		parentTracker = null;
+		System.out.println("Node Added Successfully");
+	}
+
+	public Node locateParentNode(t data){
+		Node nodeReference = root;
+		Node parentTracker;
+		while(nodeReference!=null){
+			parentTracker = nodeReference;
+			if (nodeReference.getData() == data) {
+				return parentTracker;
+			}
+			else if (nodeReference.getData()>=data){
+				nodeReference = nodeReference.left;
+			}
+			else{
+				nodeReference = nodeReference.right;
+			}
+		}
+		return null;
+	}
+
+	public Node locateNode(t data){
+		Node nodeReference = root;
+		while(nodeReference!=null){
+			if (nodeReference.getData() == data) {
+				return nodeReference;
+			}
+			else if (nodeReference.getData()>=data){
+				nodeReference = nodeReference.left;
+			}
+			else{
+				nodeReference = nodeReference.right;
+			}
+		}
+		return null;
+	}
+
+	public boolean searchNode(t data){
+		Node locationResult = 	locateNode(data);
+		if(locateResult!=null){
+			return true; //Search hit
+		}
+		else{
+			return false; //Search miss.
+		}
+	}
+
+	public boolean updateNodeValue(t currentData, t newData){
+		Node locateResult = locateNode(currentData);
+		if(locateResult!=null){
+			locateResult.setData(newData);
+			return true; //Report true that the node value was successfully updated.
+		}
+		else{
+			return false; //Report a node not found.
+		}	
+	}
+
+	public boolean deleteNode(t data){
+		Node locateResult = locateParentNode(data);
+		if(locateResult!=null){
+			if(locateResult.left.getData() == data){
+				locateResult.left == null;
+			}
+			else{
+				locateResult.right == null;
+			}
+			return true; //Report true that the node value was successfully deleted.
+		}
+		else{
+			return false; //Report a node not found.
+		}	
+	}
+
 }
 
-class node<T> {
+class Node<T> {
 	private T data;
-	public node<T> left;
-	public node<T> right;
+	public Node<T> left;
+	public Node<T> right;
 
 	public void setData(T data){
 		this.data = data;
@@ -40,4 +141,5 @@ class node<T> {
 	public T getData(){
 		return data;
 	}
+
 }
